b188d85e6f24ee3773c458496774a83212a434a4
Mariam Frimpomaa Adusei <101450922+Blacklov@users.noreply.github.com>
2023-05-14 21:54:56 +0000

    Delete 0x07-react_redux_action_creator_normalizr directory

--- 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
+++
@@ -1,7 +0,0 @@
-import * as reduxNoti  from '../../notifications.json';
-
-function getAllNotificationsByUser(userId) {
-    return reduxNoti.default.filter((contextObj) => contextObj.author.id === userId).map(({ context }) => context);
-}
-
-export default getAllNotificationsByUser;
